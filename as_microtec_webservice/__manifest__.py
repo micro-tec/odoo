ef024dbef9e2970cd2e5d0da0d09a91ae435a235
Carlos Paz <phoenixsampras@gmail.com>
2021-09-14 12:55:40 +0000

    MICROTEC 7.0

--- as_microtec_webservice/__manifest__.py
+++ as_microtec_webservice/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name' : "TIAMERICA MICROTEC Webservices",
-    'version' : "1.1.4",
+    'version' : "1.1.5",
     'author'  : "TIAMERICA",
     'description': """
 Webservice Microtec
@@ -20,6 +20,7 @@ Webservice Microtec
         'views/as_stock_warehouse.xml',
         'views/as_res_partner_views.xml',
         'views/as_product_template_views.xml',
+        'views/as_account_move.xml',
              ],
     'demo' : [],
     'installable': True,
